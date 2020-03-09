6b3698c9315fb4bac96bef901043560dc5defe93
chenzhijie <chenzhijie@onchain.com>
2020-03-09 18:04:51 +0800

    fix channel is nil pointer crash problem
    Signed-off-by: chenzhijie <chenzhijie@onchain.com>

--- dsp/channel.go
+++ dsp/channel.go
@@ -10,6 +10,7 @@ import (
 	ch_actor "github.com/saveio/pylons/actor/server"
 	"github.com/saveio/pylons/common"
 	chainCom "github.com/saveio/themis/common"
+	"github.com/saveio/themis/common/log"
 )
 
 // SetUnitPriceForAllFile. set unit price for block sharing for all files
@@ -43,10 +44,18 @@ func (this *Dsp) GetChannelPid() *actor.PID {
 }
 
 func (this *Dsp) NetworkProtocol() string {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return ""
+	}
 	return this.channel.NetworkProtocol()
 }
 
 func (this *Dsp) StartService() error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.StartService()
 }
 
@@ -55,30 +64,59 @@ func (this *Dsp) HasChannelInstance() bool {
 }
 
 func (this *Dsp) ChannelRunning() bool {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return false
+	}
 	return this.channel.Running()
 }
 
 func (this *Dsp) ChannelFirstSyncing() bool {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return false
+	}
 	return this.channel.FirstSyncing()
 }
 
 func (this *Dsp) GetCurrentFilterBlockHeight() uint32 {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0
+	}
 	return this.channel.GetCurrentFilterBlockHeight()
 }
 func (this *Dsp) StopService() {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return
+	}
 	this.channel.StopService()
 }
 func (this *Dsp) GetCloseCh() chan struct{} {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.GetCloseCh()
 }
 func (this *Dsp) SetChannelDB(db *store.ChannelDB) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return
+	}
 	this.channel.SetChannelDB(db)
 }
 func (this *Dsp) GetAllPartners() []string {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.GetAllPartners()
 }
 func (this *Dsp) AddChannelInfo(id uint64, partnerAddr string) error {
 	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
 		return nil
 	}
 	return this.channel.AddChannelInfo(id, partnerAddr)
@@ -90,18 +128,38 @@ func (this *Dsp) GetChannelInfoFromDB(targetAddress string) (*store.ChannelInfo,
 	return this.channel.GetChannelInfoFromDB(targetAddress)
 }
 func (this *Dsp) OverridePartners() error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.OverridePartners()
 }
 func (this *Dsp) WaitForConnected(walletAddr string, timeout time.Duration) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.WaitForConnected(walletAddr, timeout)
 }
 func (this *Dsp) ChannelReachale(walletAddr string) bool {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return false
+	}
 	return this.channel.ChannelReachale(walletAddr)
 }
 func (this *Dsp) HealthyCheckNodeState(walletAddr string) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.HealthyCheckNodeState(walletAddr)
 }
 func (this *Dsp) OpenChannel(targetAddress string, depositAmount uint64) (common.ChannelID, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0, nil
+	}
 	// add dns to health check list
 	if this.IsClient() {
 		return this.channel.OpenChannel(targetAddress, depositAmount)
@@ -123,58 +181,130 @@ func (this *Dsp) OpenChannel(targetAddress string, depositAmount uint64) (common
 }
 
 func (this *Dsp) SetChannelIsDNS(targetAddr string, isDNS bool) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.SetChannelIsDNS(targetAddr, isDNS)
 }
 
 func (this *Dsp) ChannelClose(targetAddress string) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.ChannelClose(targetAddress)
 }
 func (this *Dsp) SetDeposit(targetAddress string, amount uint64) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.SetDeposit(targetAddress, amount)
 }
 func (this *Dsp) CanTransfer(to string, amount uint64) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.CanTransfer(to, amount)
 }
 func (this *Dsp) NewPaymentId() int32 {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0
+	}
 	return this.channel.NewPaymentId()
 }
 func (this *Dsp) DirectTransfer(paymentId int32, amount uint64, to string) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.DirectTransfer(paymentId, amount, to)
 }
 func (this *Dsp) MediaTransfer(paymentId int32, amount uint64, media, to string) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.MediaTransfer(paymentId, amount, media, to)
 }
 func (this *Dsp) GetTotalDepositBalance(targetAddress string) (uint64, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0, nil
+	}
 	return this.channel.GetTotalDepositBalance(targetAddress)
 }
 func (this *Dsp) GetAvailableBalance(partnerAddress string) (uint64, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0, nil
+	}
 	return this.channel.GetAvailableBalance(partnerAddress)
 }
 func (this *Dsp) GetTotalWithdraw(partnerAddress string) (uint64, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0, nil
+	}
 	return this.channel.GetTotalWithdraw(partnerAddress)
 }
 func (this *Dsp) GetCurrentBalance(partnerAddress string) (uint64, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0, nil
+	}
 	return this.channel.GetCurrentBalance(partnerAddress)
 }
 func (this *Dsp) Withdraw(targetAddress string, amount uint64) (bool, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return false, nil
+	}
 	return this.channel.Withdraw(targetAddress, amount)
 }
 func (this *Dsp) CooperativeSettle(targetAddress string) error {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil
+	}
 	return this.channel.CooperativeSettle(targetAddress)
 }
 func (this *Dsp) SetUnitPrices(asset int32, price uint64) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return
+	}
 	this.channel.SetUnitPrices(asset, price)
 }
 func (this *Dsp) GetUnitPrices(asset int32) (uint64, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return 0, nil
+	}
 	return this.channel.GetUnitPrices(asset)
 }
 func (this *Dsp) CleanUnitPrices(asset int32) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return
+	}
 	this.channel.CleanUnitPrices(asset)
 }
 func (this *Dsp) ChannelExist(walletAddr string) bool {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return false
+	}
 	return this.channel.ChannelExist(walletAddr)
 }
 func (this *Dsp) GetChannelInfo(walletAddr string) (*ch_actor.ChannelInfo, error) {
+	if this.channel == nil {
+		log.Warnf("channel is not instance for service")
+		return nil, nil
+	}
 	return this.channel.GetChannelInfo(walletAddr)
 }
 func (this *Dsp) AllChannels() (*ch_actor.ChannelsInfoResp, error) {
